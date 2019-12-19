ee6748844656e7ea7f9d1fdb2c7c5bdb8724fc35
Jonathan McDonald <jonathanlmcd@gmail.com>
2019-12-19 16:57:53 -0700

    html-css-2 complete

---
+++ unit-2/2.1_html_css_2/divs_example/index.js
@@ -0,0 +1,10 @@
+let pageName = 'Webby'
+let nameList = ['Webby', 'Siteable', 'ClickClack', 'Website']
+let logo = document.getElementById('logo')
+let main = document.getElementById('main-name')
+
+function clickMe() {
+  let randomNum = Math.floor(Math.random() * nameList.length)
+  logo.innerText = nameList[randomNum]
+  main.innerText = nameList[randomNum]
+}
